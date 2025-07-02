8d5a237f511d6faafbc33612e59681ce1e1e57d9
Admin <dhruvtakkar0@gmail.com>
2025-07-02 07:44:34 +0530

    Final Vite config for full-stack deployment

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,25 @@
-require('dotenv').config();
-const app = require('./app');
-const connectDB = require('./config/db');
+require("dotenv").config();
+const path = require("path");
+const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
+// Serve React frontend after backend APIs
+if (process.env.NODE_ENV === "production") {
+  const __dirnamePath = path.resolve();
+  app.use(express.static(path.join(__dirnamePath, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirnamePath, "../client/build/index.html"));
+  });
+}
+
+// Connect DB and start server
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`🚀 Server is running on http://localhost:${PORT}`);
   });
 });
+
 
