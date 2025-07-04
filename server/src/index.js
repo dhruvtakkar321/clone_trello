b4a3eae016e425bb290db02f3c4b3de81c49463d
Admin <dhruvtakkar0@gmail.com>
2025-07-04 09:55:49 +0530

    fix the deployment issue

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,20 @@
 require("dotenv").config();
-const express = require("express"); // ✅ You missed this
 const path = require("path");
 const app = require("./app");
 const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
-
-// Serve React frontend after backend APIs
 const __dirnamePath = path.resolve();
-app.use(express.static(path.join(__dirnamePath, "../client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirnamePath, "../client/build/index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  const express = require("express");
+  app.use(express.static(path.join(__dirnamePath, "client/build")));
 
-// Connect DB and start server
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirnamePath, "client/build/index.html"));
+  });
+}
+
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`🚀 Server is running on http://localhost:${PORT}`);
