d403a11f480fc43607d4b6ef89643bbb8d0251c1
Admin <dhruvtakkar0@gmail.com>
2025-07-04 09:34:12 +0530

    fix the routes

--- server/src/routes/task.route.js
+++ server/src/routes/task.route.js
@@ -9,16 +9,15 @@ const {
 } = require('../controllers/task.controller');
 
 // GET all tasks for a board
-router.get('/boards/:id/task', getTasks);
+router.get('/:boardId', getTasks);         // GET /api/tasks/:boardId
 
 // POST a new task under a board
-router.post('/boards/:id/task', createTask);
+router.post('/:boardId', createTask);      // POST /api/tasks/:boardId
 
 // PUT update a task
-router.put('/task/:taskId', updateTask);
+router.put('/:taskId', updateTask);        // PUT /api/tasks/:taskId
 
 // DELETE a task
-router.delete('/task/:taskId', deleteTask);
+router.delete('/:taskId', deleteTask);     // DELETE /api/tasks/:taskId
 
 module.exports = router;
-
